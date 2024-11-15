1837d0524f9f452891a8ceaddcafa3f3cdc90d2f
Rory <rociodiaz.sistemas@gmail.com>
2024-11-15 20:28:32 -0300

    overview start

--- src/components/multistep/steps/OverviewStep.tsx
+++ src/components/multistep/steps/OverviewStep.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { RootState } from "../../../store/store";
+import { useSelector } from "react-redux";
+import { Typography } from "@mui/material";
 
 const OverviewStep: React.FC = () => {
+  const {
+    jobPosting,
+    formJobTitle,
+    formTitle,
+    formIntroduction,
+    formSkills,
+    formExperiences,
+  } = useSelector((state: RootState) => state.formData);
   return (
-    <div>
-      <h1>OverviewStep</h1>
-    </div>
+    <>
+      <Typography variant="h5">CV Name</Typography>
+      <Typography>{formTitle}</Typography>
+      <Typography variant="h5">Job Title</Typography>
+      <Typography>{formJobTitle}</Typography>
+      <Typography variant="h5">Introduction</Typography>
+      <Typography>{formIntroduction}</Typography>
+      <Typography variant="h5">Skills</Typography>
+      <Typography>{formSkills.join(", ")}</Typography>
+      <Typography variant="h5">Experiences</Typography>
+      <Typography>{formExperiences.join(", ")}</Typography>
+    </>
   );
 };
 
